c659b6081357d34266d3bd0f19cd150ec70e82e5
N1trousOxide <nshagalov@gmail.com>
2020-05-22 17:24:55 +0300

    init

---
+++ cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/nitrousoxide/Lab9.0
+# Build directory: /home/nitrousoxide/Lab9.0/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(unit_tests "/home/nitrousoxide/Lab9.0/cmake-build-debug/tests")
+set_tests_properties(unit_tests PROPERTIES  _BACKTRACE_TRIPLES "/home/nitrousoxide/Lab9.0/CMakeLists.txt;59;add_test;/home/nitrousoxide/Lab9.0/CMakeLists.txt;0;")
